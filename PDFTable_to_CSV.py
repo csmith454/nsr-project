43fa4007099fc0d29f66e0189fb2a661003638c6
Cole Smith <colesmith828@gmail.com>
2025-01-28 14:15:05 -0600

    pdfs output to csv files correctly

--- PDFTable_to_CSV.py
+++ PDFTable_to_CSV.py
@@ -22,7 +22,7 @@ def create_paths(start_y,max_y):
 # |      | 
 # |      |
 # #------#
-def to_dataframe(start_y,name_list):
+def to_pdf_dict(start_y,name_list):
     
     pdf_dict = {}
     curr_y = start_y
@@ -42,15 +42,15 @@ def to_dataframe(start_y,name_list):
 # #------#
 def extract_pages(start_y,pdf_dict):
     
-    # .extract_table() takes JSON table settings if needed
+    # .extract_table() takes JSON style table settings if needed
     
     curr_y = start_y
     table_dict = {}
     
-    for x in range(len(pdf_dict.items())):      # enter into each pdf_dict entry
+    for item in pdf_dict.items():                              # enter into each pdf_dict entry
         table_list = []
-        for page in pdf_dict[curr_y]:           # enter into each page of a particular pdf
-            table_to_add = page.extract_table() 
+        for n in range(len(pdf_dict[curr_y].pages)):           # enter into each page of a particular pdf
+            table_to_add = pdf_dict[curr_y].pages[n].extract_table() 
             table_list.append(table_to_add)
         table_dict[curr_y] = table_list 
         curr_y+=1 
@@ -61,9 +61,19 @@ def extract_pages(start_y,pdf_dict):
 # |      | 
 # |      |
 # #------#
-# def debug_output(pdf):
+def to_dataframe(start_y,table_dict):
+    
+    curr_y = start_y
+    df_dict = {}
     
     
+    for item in table_dict.items():
+        for n in range(len(table_dict[curr_y])):
+            data = table_dict[curr_y][:] 
+        df_dict[curr_y] = pd.DataFrame(data) 
+        curr_y+=1
+        
+    return df_dict
     
 # #------#
 # |      | 
@@ -73,10 +83,23 @@ def main():
     
     start_year = 2011
     max_year = 2018
-    pdf_dict = to_dataframe(start_year,create_paths(start_year,max_year))
+    
+    pdf_dict = to_pdf_dict(start_year,create_paths(start_year,max_year))
 
     table_dict = extract_pages(start_year, pdf_dict)
     
+    df_dict = to_dataframe(start_year,table_dict)
+    
+    print('Beginning CSV conversion...')
+    print('Converting ' + str(len(df_dict.keys())) + ' DataFrames to CSV')
+    path = '/home/csmith/desktop/nsr-project/NSRData_CSV/'
+    
+    for year, df in df_dict.items():
+        file_name = 'nsr_' + str(year) + '.csv'
+        full_path = os.path.join(path, file_name)
+        df.to_csv(full_path)
+        
+    print('CSV conversion done!')
 
 if __name__ == '__main__':
     main()
